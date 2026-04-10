7194ed364afffe0a2f4a300fb6bb8616915c1582
23wh1a0519 <23wh1a0519@bvrithyderabad.edu.in>
2026-04-10 20:20:08 +0530

    [HOME] configured route for edit task page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from './pages/Home';
+import QuestionPage from "./pages/QuestionPage";
 
 function App() {
   return (
     <div>
-      <Home />
-
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/edit-task/:id" element = {<QuestionPage/>}/>
+      </Routes>
+      </BrowserRouter>
       {/* TODO: Replace this placeholder with Routes */}
     </div>
   );
